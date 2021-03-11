73f9ab6b66d412df4d0087fad9304afce5c7b076
EmmaShi <42967483+BlueEmmaBlue@users.noreply.github.com>
2021-03-11 16:19:12 +0800

    fix: 更新类型选择

--- start_project/assets/scripts/ItemList.ts
+++ start_project/assets/scripts/ItemList.ts
@@ -5,46 +5,39 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
+
 const { ccclass, property } = cc._decorator;
 
-var Item = cc.Class({
-    name: 'item',
-    properties: {
-        id: '',
-        itemName: '',
-        itemPrice: 0,
-        iconSF: cc.SpriteFrame
-    }
-});
+interface tmpitem{
+    id: '',
+    itemName: '',
+    itemPrice: 0,
+    iconSF: cc.SpriteFrame
+};
 
 
 @ccclass
 export default class ItemList extends cc.Component {
 
-    @property(Item)
-    items: (typeof Item)[] = null;
+    @property()
+    items: tmpitem[] = null;
 
     @property(cc.Prefab)
     itemPrefab: cc.Prefab = null;
 
-    // @property({
-    //     type: [] 
-    // })
-    // items: (typeof Item)[] = null;
-    // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        // for (var i = 0; i < this.items.length; ++i) {
-        //     var item = cc.instantiate(this.itemPrefab);
-        //     var data = this.items[i];
-        //     this.node.addChild(item);
-        //     item.getComponent('ItemTmp').init({
-        //         id: data.id,
-        //         itemName: data.itemName,
-        //         itemPrice: data.itemPrice,
-        //         iconSF: data.iconSF
-        //     });
-        // }
+        for (var i = 0; i < this.items.length; ++i) {
+            var item = cc.instantiate(this.itemPrefab);
+            var data = this.items[i];
+            this.node.addChild(item);
+            item.getComponent('ItemTmp').init({
+                id: data.id,
+                itemName: data.itemName,
+                itemPrice: data.itemPrice,
+                iconSF: data.iconSF
+            });
+        }
     }
 
     start() {
